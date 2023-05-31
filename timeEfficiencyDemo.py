154b3f925fd92a46da5dadd73ab4835090259490
HussAreus <71257590+HussAreus@users.noreply.github.com>
2023-05-31 11:41:24 +0300

    Some more renaming

--- timeEfficiencyDemo.py
+++ timeEfficiencyDemo.py
@@ -3,6 +3,11 @@ import timeInfo
 import utils
 import networkx as nx
 
+# Display analysis resuls
+def displayDemo():
+    utils.displayBasicDiagrams("Complete-20230530203539.json", "Different density graphs", grouped=True)
+
+
 # Reuslt generator demo. <result> here is function function result
 def resultGenerator(results):
     totalBlocks = 0
@@ -16,19 +21,14 @@ def resultGenerator(results):
     }
 
 # Generate results for different graph sizes. Using Complete graph generator
-def size():
+def sizeDemo():
     graphSizeAnalysis = timeInfo.withTimeInfo(nx.connected_components)
     options = timeInfo.TimeInfoOptions(
         1, 100, 1, 100, "Complete", lambda N: [nx.complete_graph(N)], resultGenerator=resultGenerator)
     graphSizeAnalysis(options)
 
-
-# Display analysis resuls
-utils.displayBasicDiagrams("Complete-20230530203539.json", "Different density graphs", grouped=True)
-
-
 # Generate results for different edge probabilities
-def density():
+def densityDemo():
     graphDensityAnalysis = timeInfo.withTimeInfo(schmidts.earDecomposition)
     options = timeInfo.TimeInfoOptions(
         0, 1, 0.01, 100, "Density", lambda p, N: [nx.erdos_renyi_graph(N, p)], resultGenerator=schmidts.generateResult)
